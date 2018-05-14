e0f2c3b637fbb7e83662dde57ea6b595c460a388
kjarmicki <jarmicki@gmail.com>
2018-05-14 15:46:47 +0200

    tests for timeline rulers

--- src/views/timeline/Timeline.test.js
+++ src/views/timeline/Timeline.test.js
@@ -28,4 +28,29 @@ describe('The Timeline component', () => {
     expect(Rovers.at(0)).toHaveProp('bounds', { min: expectedMinBound, max: expectedMaxBound });
     expect(Rovers.at(1)).toHaveProp('bounds', { min: expectedMinBound, max: expectedMaxBound });
   });
+
+  it('should render timeline bound dates correctly', () => {
+    // given
+    const rovers = [
+      {
+        id: 1,
+        landing_date: '2012-04-06',
+        max_date: '2016-05-08',
+      },
+      {
+        id: 2,
+        landing_date: '2011-05-03',
+        max_date: '2013-09-10',
+      },
+    ];
+
+    // when
+    const component = shallow(<Timeline rovers={rovers} />);
+    const lowerBound = component.find('.timeline-ruler-lower-bound');
+    const upperBound = component.find('.timeline-ruler-upper-bound');
+
+    // then
+    expect(lowerBound).toHaveText('2011-05-03');
+    expect(upperBound).toHaveText('2016-05-08');
+  });
 });

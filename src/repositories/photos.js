2306af750ed9e9a7e6721963bb6f3501c729ce47
kjarmicki <jarmicki@gmail.com>
2018-05-18 01:02:54 +0200

    explain photos limit implementation

--- src/repositories/photos.js
+++ src/repositories/photos.js
@@ -3,6 +3,7 @@ export default function photosRepository(client) {
     async getByRoverAndEarthDate(roverName, earthDate, options = {}) {
       const { limit } = options;
       const response = await client.get(`rovers/${roverName.toLowerCase()}/photos?earth_date=${earthDate}`);
+      // unfortunately NASA API doesn't support limits, so we have to fake it here
       if (typeof limit === 'number' && !Number.isNaN(limit)) {
         return response.photos.slice(0, options.limit);
       }
